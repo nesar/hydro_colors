611f173e754739221611b6afeaafa24b7c7075c4
nesar <nramachandra@anl.gov>
2023-05-02 18:48:42 -0500

    loading library and hacc data

--- hydro_colors/_modidx.py
+++ hydro_colors/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://nesar.github.io',
                 'git_url': 'https://github.com/nesar/hydro_colors',
                 'lib_path': 'hydro_colors'},
-  'syms': {'hydro_colors.core': {'hydro_colors.core.foo': ('core.html#foo', 'hydro_colors/core.py')}}}
+  'syms': { 'hydro_colors.core': { 'hydro_colors.core._convert_age': ( 'load_hacc_gal_stellar_catalog.html#_convert_age',
+                                                                       'hydro_colors/core.py'),
+                                   'hydro_colors.core._convert_metallicity': ( 'load_hacc_gal_stellar_catalog.html#_convert_metallicity',
+                                                                               'hydro_colors/core.py'),
+                                   'hydro_colors.core.load_fsps_spectral_library': ( 'load_hacc_gal_stellar_catalog.html#load_fsps_spectral_library',
+                                                                                     'hydro_colors/core.py')}}}
